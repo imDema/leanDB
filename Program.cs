9dcedf6ce6d502d6f647b068521a2a6c491ec933
imDema <dema3112@gmail.com>
2018-05-14 13:34:13 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace leandb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public interface ILeanDB
+    {
+        
+        void Insert (ILeanDBObject obj);
+        void Delete (ILeanDBObject obj);
+        void Update (ILeanDBObject obj);
+        ILeanDBObject Find (Guid guid);
+    }
+
+    public interface ILeanDBObject
+    {
+
+    }
+}
